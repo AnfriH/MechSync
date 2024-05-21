6ff0a37749ff6925f2cfb995eba49bedc6643d1a
Anfri <haywarmatt@ecs.vuw.ac.nz>
2024-05-21 19:45:09 +1200

    Rewrite Input to use *mut pointer instead of &'static reference
    Before, we were performing UB by destructing a 'static refrerence. Now we use an unsafe pointer, which makes no such guarentees

--- src/midi.rs
+++ src/midi.rs
@@ -1,6 +1,6 @@
 use std::mem::ManuallyDrop;
 // mutex should be fine here, as we only bind from a single thread. sorry may :(
-use std::sync::{Arc, Mutex, Weak};
+use std::sync::{Mutex, Weak};
 
 use may::go;
 use may::sync::RwLock;
@@ -10,64 +10,63 @@ use midir::os::unix::{VirtualInput, VirtualOutput};
 use crate::data::MidiData;
 use crate::node::Node;
 
-struct InputImpl {
-    callback: RwLock<Option<Weak<dyn Node>>>
+#[derive(Copy, Clone)]
+struct InputCallback {
+    ptr: *mut RwLock<Option<Weak<dyn Node>>>
 }
 
-impl InputImpl {
-    fn new() -> Self {
-        InputImpl {
-            callback: RwLock::new(None),
+unsafe impl Sync for InputCallback {}
+unsafe impl Send for InputCallback {}
+
+impl InputCallback {
+    unsafe fn call(&self, data: MidiData) {
+        if let Some(callback) = self.ptr.as_ref().unwrap().read().unwrap().as_ref() {
+            callback.upgrade().unwrap().call(data);
         }
     }
 
-    fn bind(&self, node: Weak<dyn Node>) {
-        let mut callback = self.callback.write().unwrap();
-        let _prev = callback.insert(node);
-    }
-
-    fn call(&self, data: MidiData) {
-        let callback = self.callback.read().unwrap();
-        if let Some(cb) = callback.as_ref().and_then(|w| w.upgrade()) {
-            cb.call(data);
-        }
+    unsafe fn bind(&self, node: Weak<dyn Node>) {
+        let _ = self.ptr.as_ref().unwrap().write().unwrap().insert(node);
     }
 }
 
 pub(crate) struct Input {
     connection: ManuallyDrop<Mutex<MidiInputConnection<()>>>,
-
-    // NOTE: binding must never be leaked, otherwise reference to freed memory will occur
-    binding: &'static InputImpl
+    ptr: InputCallback
 }
 
 impl Input {
     pub(crate) fn new(name: &str) -> Result<Self, ConnectError<MidiInput>> {
         let backing = MidiInput::new(name).unwrap();
 
-        let binding = Box::leak(Box::new(InputImpl::new()));
+        let binding = InputCallback { ptr: Box::into_raw(Box::new(RwLock::new(None)))};
+        let binding_cpy = binding;
 
         let input = Input {
-            connection: ManuallyDrop::new(Mutex::new(backing.create_virtual(name, |ts, data, _| {
+            connection: ManuallyDrop::new(Mutex::new(backing.create_virtual(name, move |ts, data, _| {
                 let md = MidiData::from_slice(ts, data);
-                go!(|| {
-                    binding.call(md);
+                let binding_cpy = binding_cpy;
+                go!(move || unsafe{
+                    binding_cpy.call(md);
                 });
             }, ())?)),
-            binding
+            ptr: binding,
         };
 
         Ok(input)
     }
 
     pub(crate) fn bind(&self, node: Weak<dyn Node>) {
-        self.binding.bind(node)
+        unsafe {
+            self.ptr.bind(node);
+        }
     }
 }
 
 impl Node for Input {
-    fn call(&self, data: MidiData) -> () {
-        self.binding.call(data)
+    // NOTE: you probably didn't want to call this
+    fn call(&self, _data: MidiData) -> () {
+        //TODO: what should we do here?
     }
 }
 
@@ -77,8 +76,8 @@ impl Drop for Input {
             // we must explicitly drop the midi connection first
             ManuallyDrop::drop(&mut self.connection);
 
-            // then we must turn our &'static into a *mut to properly deconstruct
-            let _ = Box::from_raw(self.binding as *const InputImpl as *mut InputImpl);
+            // then destroy the ptr
+            let _ = Box::from_raw(self.ptr.ptr);
         }
     }
 }
